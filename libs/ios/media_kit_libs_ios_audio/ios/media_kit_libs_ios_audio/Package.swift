ee73bb301bdc05f242f1f4bec4dc2a9599b38061
Roman Laptev <carapacik@gmail.com>
2026-05-08 16:05:45 +0300

    Real checksum

--- libs/ios/media_kit_libs_ios_audio/ios/media_kit_libs_ios_audio/Package.swift
+++ libs/ios/media_kit_libs_ios_audio/ios/media_kit_libs_ios_audio/Package.swift
@@ -18,16 +18,16 @@ let libmpvTargets = [
 
 let libmpvArtifactBase = "https://github.com/media-kit/libmpv-darwin-build/releases/download/v0.7.0/libmpv-xcframeworks_v0.7.0_ios-universal-audio-default"
 let libmpvChecksums = [
-    "Avcodec": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avfilter": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avformat": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avutil": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedcrypto": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedtls": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedx509": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mpv": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Swresample": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Swscale": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa"
+    "Avcodec": "fc72195ca32ed023a6230e4e558323fdb14d8e779b7c68cf97ed56ac6d00aebb",
+    "Avfilter": "e4d71789a98a9f0d60d83de15b7facf6bcf5914f25a6614e8b956296899a99d7",
+    "Avformat": "4abb7dd215203a22caaabb215b0b2f5e10fd1fe0c8a1ae874908eeea8859eb1e",
+    "Avutil": "63573305de494575ca29b4bfe328997c0e844132d9e4b6bf8c22ffd4f3cf6273",
+    "Mbedcrypto": "b016619250601bac48c1b628f958d352d58716bf74fcb6168d7c0ef7b1a78e4d",
+    "Mbedtls": "457264fdcdaea78ff643c5d3fa184876f955fe202471b7f2b856fb698794269e",
+    "Mbedx509": "1cde5fa0f6d5308d3a012ce4925551aaad0449e63af82de2ee509970645e96e6",
+    "Mpv": "0c052ddcb71ab19bd1ae2bbc74514bbfad2ef71eb7b41468c7e71ee9cf7dd2dd",
+    "Swresample": "5ddbeb5a57a59e87d3b62f33d6340fbcdc35a3dc972064d53c87b9cabf97b4fc",
+    "Swscale": "302507bbd858e5fc84a88a441ae4b14d577a5a6bd8119ed2e2367468fc9a9dbb"
 ]
 
 let package = Package(
--- libs/macos/media_kit_libs_macos_audio/macos/media_kit_libs_macos_audio/Package.swift
+++ libs/macos/media_kit_libs_macos_audio/macos/media_kit_libs_macos_audio/Package.swift
@@ -18,16 +18,16 @@ let libmpvTargets = [
 
 let libmpvArtifactBase = "https://github.com/media-kit/libmpv-darwin-build/releases/download/v0.7.0/libmpv-xcframeworks_v0.7.0_macos-universal-audio-full"
 let libmpvChecksums = [
-    "Avcodec": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avfilter": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avformat": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Avutil": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedcrypto": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedtls": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mbedx509": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Mpv": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Swresample": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa",
-    "Swscale": "aaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aaaaa111aa"
+    "Avcodec": "1372bdc0fa7f4ded6b565d29023aba08dd0ed9d6b1916bd2f03409c6370f4e1d",
+    "Avfilter": "8c471be58b680ae3ac60ffef3ea166447adcd40f660b051a6e4f59aecdd45893",
+    "Avformat": "3bb73535b43a1cc992777c3b017eb5a84bf4d0ae437c9250ff8511a161617a46",
+    "Avutil": "3d65e0b2bd65cad4c8a639194398753329dc657c2f418a9c7c662ec8947d76d4",
+    "Mbedcrypto": "1107eef2c02409840b5f51f9333e527e027aef26f2246af5831f489d50eb6ba0",
+    "Mbedtls": "f9e8b88cf029b1d8c3093fb3c3150ebc8815f05b7ca259950a1c05e31c7ac532",
+    "Mbedx509": "8697ad3585f41fb8b8806efaabdd6ff1fa617c7b5a05c13aa86152902ae12917",
+    "Mpv": "6f5d7fe8b31235d28d8a5fb97b801f0cfc3f19ccad7ac2cb9100f699aafb95e3",
+    "Swresample": "87c53bd728d0406624c782497a0ef971d28226a84f67997c1891db8bc6c2cc12",
+    "Swscale": "ea7786b057662edc505696bd9dbbb8bc48feadd869eee1d8eaeab8b2da15d9ed"
 ]
 
 let package = Package(
